ddcee0b81c59d3192ba9589388c33ec547296434
jemila <abubakarjemiletu08@gmail.com>
2025-09-05 04:42:20 +0100

    reset taiwind

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
+
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/cdtheclienttt/', // This should match your repository name
+  base: '/cdtheclienttt/', // Make sure this matches your GitHub repo name
   plugins: [
     react(),
-    tailwindcss()
   ],
+  css: {
+    postcss: './postcss.config.js',
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
